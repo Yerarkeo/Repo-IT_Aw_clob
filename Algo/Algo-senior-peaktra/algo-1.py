b21042ee877e169d36c3dee74544db1cafb09c5d
SREYDOEURN.KEO <keoyerar@gmail.com>
2025-06-23 07:38:42 +0700

    put new code

---
+++ Algo/Algo-senior-peaktra/algo-1.py
@@ -0,0 +1,183 @@
+number=int(input('put your number:'))
+while number!=9:
+    print('try again')
+    number=int(input('put your number:'))
+print('enter your number')
+text='AbbaBAaAb'
+counter=0
+count=0
+for i in range(len(text)):
+    if text[i]=='a' or text[i]=='A':
+        counter+=1
+    if text[i]=='b':
+        count+=1
+print('A and a:',counter)
+print('b:',count)
+texts='AbbaBAaAb'
+counter=0
+count=0
+for text in texts:
+    if text. lower()=='a':
+        counter+=1
+    if text=='b':
+        count+=1
+print('A and a:',counter,'b:',count)
+texts='AaBabaCcDdafe'
+counter=0
+for text in texts:
+    if text.lower()=='b' or text.lower()=='c' or text.lower()=='d':
+        counter+=1
+print('total:',counter)
+texts='AaBabaCcDdafe'
+count=0
+for text in texts:
+    if text.upper()=='B':
+        count+=1
+    elif text.upper()=='C':
+        count+=1
+    elif text.upper()=='D':
+        count+=1
+print('total',count)    
+texts=input('enter your text')
+result=''
+for i in range(len(texts)):
+    if texts[i].lower()=='c':
+        result+=str(i) +'-'
+print(result[:-1])
+# # ex1:
+number='1234'
+sum=0
+for num in number:
+      sum+=int(num)
+print(sum)
+# ex2:
+texts=input('Enter your letter:')
+while texts.lower()!='a': 
+        print('Try again!')
+        texts=input('Enter your letter:')
+print('found letter A') 
+print(('Enter your letter:'))
+# ex4:
+number=int(input('Enter your number:'))
+while number!=5:
+    print('Try  more')
+    number=int(input('Enter your number'))
+print('found number 5 !')
+# ex5:
+arr2D=[
+    [1,2,3,],
+    [4,5,6],
+    [7,8,9]
+    ]
+number=arr2D[0]
+for num in number:
+    print(num)
+    number=int(input('put your number:'))
+while number!=9:
+    print('try again')
+    number=int(input('put your number:'))
+print('enter your number')
+text='AbbaBAaAb'
+counter=0
+count=0
+for i in range(len(text)):
+    if text[i]=='a' or text[i]=='A':
+        counter+=1
+    if text[i]=='b':
+        count+=1
+print('A and a:',counter)
+print('b:',count)
+texts='AbbaBAaAb'
+counter=0
+count=0
+for text in texts:
+    if text. lower()=='a':
+        counter+=1
+    if text=='b':
+        count+=1
+print('A and a:',counter,'b:',count)
+texts='AaBabaCcDdafe'
+counter=0
+for text in texts:
+    if text.lower()=='b' or text.lower()=='c' or text.lower()=='d':
+        counter+=1
+print('total:',counter)
+texts='AaBabaCcDdafe'
+count=0
+for text in texts:
+    if text.upper()=='B':
+        count+=1
+    elif text.upper()=='C':
+        count+=1
+    elif text.upper()=='D':
+        count+=1
+print('total',count)    
+texts=input('enter your text')
+result=''
+for i in range(len(texts)):
+    if texts[i].lower()=='c':
+        result+=str(i) +'-'
+print(result[:-1])
+number=int(input('put your number:'))
+while number!=9:
+    print('try again')
+    number=int(input('put your number:'))
+print('enter your number')
+text='AbbaBAaAb'
+counter=0
+count=0
+for i in range(len(text)):
+    if text[i]=='a' or text[i]=='A':
+        counter+=1
+    if text[i]=='b':
+        count+=1
+print('A and a:',counter)
+print('b:',count)
+texts='AbbaBAaAb'
+counter=0
+count=0
+for text in texts:
+    if text. lower()=='a':
+        counter+=1
+    if text=='b':
+        count+=1
+print('A and a:',counter,'b:',count)
+texts='AaBabaCcDdafe'
+counter=0
+for text in texts:
+    if text.lower()=='b' or text.lower()=='c' or text.lower()=='d':
+        counter+=1
+print('total:',counter)
+texts='AaBabaCcDdafe'
+count=0
+for text in texts:
+    if text.upper()=='B':
+        count+=1
+    elif text.upper()=='C':
+        count+=1
+    elif text.upper()=='D':
+        count+=1
+print('total',count)    
+texts=input('enter your text')
+result=''
+for i in range(len(texts)):
+    if texts[i].lower()=='c':
+        result+=str(i) +'-'
+print(result[:-1])
+text=input('enter your text:')
+result=''
+for i in range(len(text)):
+    if i%2==0:
+        result=result + text[i]
+print(result)
+text='keoyerar'
+counter=''
+for i in range(len(text)):
+    if i%2==0:
+        counter+= text[i]
+print(counter)
+text=input('enter your text')
+
+
+
+
