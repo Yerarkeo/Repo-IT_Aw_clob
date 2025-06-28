dc22563fec9c5ae7ce62f3ec366fe4bc042cd517
SREYDOEURN.KEO <keoyerar@gmail.com>
2025-06-28 17:59:08 +0700

    push new code

---
+++ senioer_poleak/day1function.py
@@ -0,0 +1,67 @@
+# def sum(num1,num2):
+#     return num1+num2
+# num1=20
+# num2=5
+# num3=3
+# num4=7
+# print(sum(num1,num2))
+# print(sum(num3,num4))
+# # ex2:
+# num1=20
+# num2=5
+# print(num1+num2)
+
+# # Practice:
+# 1:
+
+# def sum_number(num1,num2):
+#     return num1+num2
+# def minus_number(num1,num2):
+#     return num1-num2
+# def malti_number(num1,num2):
+#     return num1*num2
+# def devide_number(num1,num2):
+#     return num1/num2
+# num1=30
+# num2=15
+# sum=(sum_number(num1,num2))+(minus_number(num1,num2))+(malti_number(num1,num2))+(devide_number(num1,num2))
+# print(sum)
+# # print(minus_number(num1,num2))
+# # print(sum_number(num1,num2))
+# # print(malti_number(num1,num2))
+# # print(devide_number(num1,num2))
+
+# # 2:
+# def sumNum(num1,num2):
+#     return num1+num2
+# def minusNum(num1,num2):
+#     return num1-num2
+# def devideNum(num1,num2):
+#     return num1/num2
+# def multiNum(num1,num2):
+#     return num1*num2
+# num1=30
+# num2=15
+# sumnum=sumNum(num1,num2)
+# minus=minusNum(num1,num2)
+# devide=devideNum(num1,num2)
+# multi=multiNum(num1,num2)
+# sumAll= sumnum + minus + devide + multi
+# print(sumAll)
+
+# countletter:
+
+# def count_letter(word):
+#     count=0
+#     for i in range(len(word)):
+#          count+=1
+#     return count
+# word1='study'
+# word2='hello'
+# word3='sushi'
+
+# print(count_letter(word1))
+# print(count_letter(word2))
+# print(count_letter(word3))
+# print(count_letter(word1),count_letter(word2),count_letter(word3))
+  
